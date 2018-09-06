14025f6438f5eab562edf933c5ea76622f684879
wildlifehexagon <crossthebreeze@gmail.com>
2018-09-06 17:04:59 -0500

    test: add test for ToolbarButton, add flow-typed definitions

---
+++ src/editor/toolbar/ToolbarButton.test.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { mount } from "enzyme"
+import ToolbarButton from "./ToolbarButton"
+import Button from "@material-ui/core/Button"
+
+describe("editor/toolbar/ToolbarButton", () => {
+  const wrapper = mount(<ToolbarButton />)
+
+  describe("initial render", () => {
+    it("always renders an Button", () => {
+      expect(wrapper.find(Button).length).toBe(1)
+    })
+  })
+})
