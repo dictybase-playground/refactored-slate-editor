878b468042b56d4bed5535fb5d925935d42d1354
wildlifehexagon <crossthebreeze@gmail.com>
2018-09-06 16:35:20 -0500

    test: add test for PageEditor

---
+++ src/editor/PageEditor.test.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { shallow } from "enzyme"
+import PageEditor from "./PageEditor"
+import { Editor } from "slate-react"
+
+describe("editor/PageEditor", () => {
+  const wrapper = shallow(<PageEditor />)
+
+  describe("initial render", () => {
+    it("always renders an Editor", () => {
+      expect(wrapper.find(Editor).length).toBe(1)
+    })
+  })
+})
