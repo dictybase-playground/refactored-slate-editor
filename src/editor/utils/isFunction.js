7c78c02b2ba54e4a4b39c4d6823f491acfa59694
wildlifehexagon <crossthebreeze@gmail.com>
2018-09-07 10:25:17 -0500

    refactor: refactor isFunction util and write documentation for these helpers

--- src/editor/utils/isFunction.js
+++ src/editor/utils/isFunction.js
@@ -1,1 +1,10 @@
-export const isFunction = obj => !!(obj && obj.constructor === Function)
+/**
+ * Helper function that checks if a given item is a function.
+ */
+
+export const isFunction = v => {
+  if (v instanceof Function) {
+    return true
+  }
+  return false
+}
--- src/editor/utils/isMod.js
+++ src/editor/utils/isMod.js
@@ -1,1 +1,6 @@
+/**
+ * Helper function that checks if the meta or ctrl key is pressed.
+ * metaKey = ⌘ for Mac, ⊞ for Windows
+ */
+
 export const isMod = event => (event.metaKey && !event.ctrlKey) || event.ctrlKey
