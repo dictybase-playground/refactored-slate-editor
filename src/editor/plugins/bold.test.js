b56c04351f0defcdd0584a4488a2f25c6d555cdf
wildlifehexagon <crossthebreeze@gmail.com>
2018-09-10 17:16:33 -0500

    test: add first tests for alignment, list plugins

--- src/editor/plugins/bold.test.js
+++ src/editor/plugins/bold.test.js
@@ -1,12 +1,9 @@
 import React from "react"
 import { shallow } from "enzyme"
-// import { Value } from "slate"
-// import { BeforePlugin, AfterPlugin } from "slate-react"
-// import Simulator from "slate-simulator"
+import { Value } from "slate"
 
 import { BoldButton, BoldPlugin, BoldMark } from "./bold"
 import ToolbarButton from "editor/toolbar/ToolbarButton"
-import initialValue from "editor/data/initialValue.json"
 
 describe("editor/plugins/bold", () => {
   describe("BoldMark", () => {
@@ -41,23 +38,6 @@ describe("editor/plugins/bold", () => {
       // wrapper.find(ToolbarButton).simulate("click")
       // wrapper.debug()
     })
-
-    // test("bold plugin makes text bold", () => {
-    //   const simulator = new Simulator({
-    //     value: Value.fromJSON(initialValue),
-    //     plugins: [BeforePlugin(), BoldPlugin(), AfterPlugin()],
-    //   })
-    //   simulator
-    //     .focus()
-    //     .beforeInput({ data: "H" })
-    //     .beforeInput({ data: "e" })
-    //     .beforeInput({ data: "l" })
-    //     .beforeInput({ data: "l" })
-    //     .beforeInput({ data: "o" })
-    //     .keyDown({ key: "Enter" })
-
-    //   expect(simulator.value.toJSON()).toEqual(valueGenerator("Hello"))
-    // })
   })
 
   describe("BoldKeyboardShortcut", () => {
